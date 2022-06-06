d53afeb96396790bc2808f1cb28158e88127c8b1
wmzxc <opxlt61445@yahoo.co.jp>
2022-06-06 17:46:36 +0900

    first commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.webpacker.check_yarn_integrity=false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
